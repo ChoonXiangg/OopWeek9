2ecec4625aa18ff6a6c74dc7ddc5c4550f14ee37
ChoonXiang <g00425056@atu.ie>
2023-11-20 17:16:50 +0000

    Added class PrintWriter

--- src/main/java/ie/atu/WriteFile.java
+++ src/main/java/ie/atu/WriteFile.java
@@ -1,16 +1,22 @@
 package ie.atu;
 import java.io.IOException;
 import java.io.FileWriter;
+import java.io.PrintWriter;
 public class WriteFile {
     public static void main(String[] args) {
         String fileName="File13.txt";
         try{
             FileWriter addText=new FileWriter(fileName,true);
-            addText.write("Hello world\n");
+            addText.write("Hello world");
+            addText.close();
+            System.out.println("Content successfully written to the file");
+            PrintWriter addText1=new PrintWriter(fileName);
+            addText1.write("I am Rick");
+            addText.close();
             System.out.println("Content successfully written to the file");
         }
         catch(IOException e){
-            System.out.println("An error occurred while creating the file.");
+            System.out.println("An error occurred while writing the file.");
             e.printStackTrace();
         }
     }
